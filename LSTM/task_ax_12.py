1677ec568fd9e0c0b815598796210506001b2b12
zenggo <zenggo1993@gmail.com>
2020-05-21 20:10:03 +0100

    delete train&test timing; add avg_acc and avg_f1 (macro) in train&test statistics

--- LSTM/task_ax_12.py
+++ LSTM/task_ax_12.py
@@ -8,12 +8,13 @@ torch.manual_seed(123)
 
 env = gym.make('AX_12-v0', size=10, prob_target=0.5)
 
-N_tr = 300
-N_tst = 200
+N_tr = 200
+N_tst = 100
 n_hidden = 20
+n_layers = 1
 lr = 0.01
 
-agent = Agent_LSTM(env.observation_space.n, env.action_space.n, n_hidden, lr, n_layers=1)
+agent = Agent_LSTM(env.observation_space.n, env.action_space.n, n_hidden, lr, n_layers)
 
 train(env, agent, N_tr, seed=123)
 test(env, agent, N_tst)
--- LSTM/task_seq_pred.py
+++ LSTM/task_seq_pred.py
@@ -2,33 +2,32 @@ import gym
 import gym_cog_ml_tasks
 from LSTM.LSTM_model import Agent_LSTM
 from common.utils import train, test, save_train_res, load_train_res, train_results_plots
+import torch
+torch.manual_seed(123)
 
+env = gym.make('seq_prediction-v0', size=30, p=0.5)
 
-env = gym.make('seq_prediction-v0', size=10, p=0.5)
-
-N_tr = 300
+N_tr = 100
 N_tst = 100
-n_hidden = 10
+n_hidden = 20
+n_layers = 1
 lr = 0.01
-agent = Agent_LSTM(env.observation_space.n, env.action_space.n, n_hidden, lr)
+agent = Agent_LSTM(env.observation_space.n, env.action_space.n, n_hidden, lr, n_layers)
 
-res_10 = train(env, agent, N_tr, print_progress=True, seed=123)
-save_train_res('./save/seq_pred/LSTM_10_res', res_10)
+res_1 = train(env, agent, N_tr, print_progress=True, seed=123)
+save_train_res('./save/seq_pred/LSTM_l1_res', res_1)
 # agent.save(dir='./save/seq_pred', name='LSTM_10')
 # agent.load('./save/seq_pred/LSTM_10')
-# test(env, agent, N_tst, print_progress=True, seed=123)
-# a = load_train_res('./save/seq_pred/LSTM_10_res.npy')
+test(env, agent, N_tst, print_progress=True, seed=123)
+# a = load_train_res('./save/seq_pred/LSTM_l1_res.npy')
 
 
-N_tr = 300
-N_tst = 100
-n_hidden = 30
-lr = 0.01
-agent = Agent_LSTM(env.observation_space.n, env.action_space.n, n_hidden, lr)
+n_layers = 2
+agent = Agent_LSTM(env.observation_space.n, env.action_space.n, n_hidden, lr, n_layers)
 
-res_30 = train(env, agent, N_tr, print_progress=True, seed=123)
-save_train_res('./save/seq_pred/LSTM_30_res', res_30)
+res_2 = train(env, agent, N_tr, print_progress=True, seed=123)
+save_train_res('./save/seq_pred/LSTM_l2_res', res_2)
 # agent.save(dir='./save/seq_pred', name='LSTM_30')
 # test(env, agent, N_tst, print_progress=True, seed=123)
 
-train_results_plots(dir='./save/seq_pred', figname='LSTM', names=['LSTM_10', 'LSTM_30'], numbers=[res_10, res_30])
+train_results_plots(dir='./save/seq_pred', figname='LSTM', names=['LSTM_l1', 'LSTM_l2'], numbers=[res_1, res_2])
